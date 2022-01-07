0a8744fc480356e4b5edb643a9ec5c617ac3d82c
AsafSivan1 <asafisivan@gmail.com>
2022-01-07 17:36:35 +0200

    fixed hebrew issue

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -10,12 +10,12 @@ export default function Team() {
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(teachers);
+  const [classes, setClasses] = useState(["ז", "ח", "ט", "י", "יא", "יב"]);
   return (
     <div className="flex items-center justify-center py-8 px-4">
-      <div className="max-w-lg w-full shadow-lg">
+      <div className="max-w-lg w-full shadow-lg right">
         {teachers.map((teacher, index) => (
-          <div className="inliner">
+          <div className="inliner" id={index}>
             <img
               className="rounded-full px-4 py-4"
               src={`https://ui-avatars.com/api/?name=${teacher.firstName}+${teacher.lastName}&size=200&background=fff&color=5f72e4`}
@@ -27,10 +27,7 @@ export default function Team() {
               </p>
             </div>
             <div className="text-center">
-              <p className="text-lg">
-                {" "}
-                יועצת שכבה {String.fromCharCode(index + 1494)}
-              </p>
+              <p className="text-lg"> יועצת שכבה {classes[index]}'</p>
             </div>
           </div>
         ))}
