52d85fbff9f867f14950c61e065e40c58c0c957e
AsafSivan1 <asafisivan@gmail.com>
2022-01-13 14:33:50 +0200

    sort by grade in /Team

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -11,6 +11,8 @@ export default function Team() {
   }, []);
 
   const [classes, setClasses] = useState(["ז", "ח", "ט", "י", "יא", "יב"]);
+  teachers.sort((a, b) => (a.grade > b.grade ? 1 : -1));
+  console.log(teachers);
   return (
     <div className="flex items-center justify-center py-8 px-4">
       <div className="max-w-lg w-full shadow-lg right  text-white ">
