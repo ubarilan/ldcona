7713251791989fec0b5ba0dd19e4a7820ef8ca71
AsafSivan1 <asafisivan@gmail.com>
2022-01-13 22:29:29 +0200

    changed the teacher in /Team into a component

---
+++ frontend/src/components/Teacher.jsx
@@ -0,0 +1,21 @@
+import { React } from "react";
+
+export default function Teacher({ grade, index, teacher }) {
+  return (
+    <div className="inliner" id={index}>
+      <img
+        className="rounded-full px-4 py-4"
+        src={`https://ui-avatars.com/api/?name=${teacher.firstName}+${teacher.lastName}&size=200&background=fff&color=5f72e4`}
+        alt=""
+      />
+      <div className="text-center">
+        <p className="text-2xl">
+          {teacher.firstName} {teacher.lastName}
+        </p>
+      </div>
+      <div className="text-center">
+        <p className="text-lg"> יועצת שכבה {grade} '</p>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from "react";
+import Teacher from "@Components/Teacher";
 
 export default function Team() {
   const [teachers, setTeachers] = useState([{}]);
@@ -17,21 +18,7 @@ export default function Team() {
     <div className="flex items-center justify-center py-8 px-4">
       <div className="max-w-lg w-full shadow-lg right  text-white ">
         {teachers.map((teacher, index) => (
-          <div className="inliner" id={index}>
-            <img
-              className="rounded-full px-4 py-4"
-              src={`https://ui-avatars.com/api/?name=${teacher.firstName}+${teacher.lastName}&size=200&background=fff&color=5f72e4`}
-              alt=""
-            />
-            <div className="text-center">
-              <p className="text-2xl">
-                {teacher.firstName} {teacher.lastName}
-              </p>
-            </div>
-            <div className="text-center">
-              <p className="text-lg"> יועצת שכבה {classes[index]}'</p>
-            </div>
-          </div>
+          <Teacher index={index} teacher={teacher} grade={classes[index]} />
         ))}
       </div>
     </div>
