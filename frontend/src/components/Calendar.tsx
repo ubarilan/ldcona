b69ddf0accf40b8038fb30d8ab87274b6af695c3
asaf <asaf@DESKTOP-4QIPBBP.lan>
2022-01-28 11:10:16 +0200

    Added calendar + meetings page

---
+++ frontend/src/components/Calendar.tsx
@@ -0,0 +1,28 @@
+import Calendar from 'react-calendar';
+import React, { useState } from 'react';
+
+export default function CalendarComponent() {
+    const [value, onChange] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const mark = ['1/1/2022', '01-04-2022', '01-03-2022'];
+
+    return (
+        <div>
+            <Calendar
+                className="font-bold text-lg"
+                onChange={onChange}
+                value={value}
+                onClickDay={(value) => {
+                    console.log(selectedDate);
+                    setSelectedDate(value);
+                }}
+                tileClassName={({ date, view }) => {
+                    if (selectedDate.getTime() === date.getTime()) {
+                        return 'bg-green-500 text-white';
+                    }
+                }}
+            />
+        </div>
+    );
+}
---
+++ frontend/src/pages/meetings.tsx
@@ -0,0 +1,13 @@
+import CalendarComponent from "../components/Calendar";
+
+
+export default function Meetings () {
+    return (
+        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+            <div className="text-center">
+               <CalendarComponent /> 
+            </div>
+        </main>
+    );
+    
+}
