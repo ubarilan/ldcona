807ee22137d775aa43bf1d4269ad2f45d7b9547e
ubarilan <ubarilan@gmail.com>
2022-03-08 13:28:35 +0200

    Added a dot

--- frontend/src/components/MonthDay.tsx
+++ frontend/src/components/MonthDay.tsx
@@ -2,7 +2,7 @@ export default function MonthDay({ change, number, isSelected, hasTasks }) {
     function handleClick(): void {
         change(number);
     }
-
+    console.log(hasTasks);
     return (
         <td onClick={() => handleClick()} className="pt-6">
             {isSelected ? (
@@ -20,7 +20,7 @@ export default function MonthDay({ change, number, isSelected, hasTasks }) {
             ) : (
                 <div
                     className={`px-2 py-2 cursor-pointer flex w-full justify-center rounded-full hover:bg-indigo-100 ${
-                        hasTasks && 'day-with-tasks'
+                        hasTasks && 'day-with-task'
                     }`}
                 >
                     <p className="text-base text-gray-900 font-medium">
